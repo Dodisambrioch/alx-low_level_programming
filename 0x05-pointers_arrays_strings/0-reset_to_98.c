8f304e9a5ec50b5831af1105ac73a49076b7fa08
Dodisambrioch <dundosambrioch@gmail.com>
2023-06-27 12:08:54 -0700

    added

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - check the code for main School students.
+ * @n: is a pointer int type
+ * Return: Always 0.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * _strlen - check the code for main School students.
+ * @s: is a pointer int char type
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+	int len;
+
+	for (len = 0; *s != '\0'; len++)
+
+		s++;
+
+	return (len);
+}
