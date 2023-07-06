fd7ef12284736c697abbf4a0193728e9bbc02114
Dodisambrioch <dundosambrioch@gmail.com>
2023-07-06 13:10:17 -0700

    The Technical University of Kenya

---
+++ 0x08-recursion/4-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	    int r;
+
+	r = _pow_recursion(5, 10);
+	printf("%d\n", r);
+	return (0);
+}
