fbb38779c1acc4eea999b0e0f0ca25bccc940fb7
Dodisambrioch <dundosambrioch@gmail.com>
2023-07-18 11:31:21 -0700

    pius added

---
+++ 0x0E-structures_typedef/5-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	dog_t *my_dog;
+
+	my_dog = new_dog("Poppy", 3.5, "Gabby");
+	printf("I am %s and am %.1f :)Woof!\n", my_dog->name, my_dog->age);
+	free_dog(my_dog);
+	return (0);
+}
