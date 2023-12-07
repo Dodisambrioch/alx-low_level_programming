dd9a3410eab55a6213eb8a0ff0491c9b27c810c4
Dodisambrioch <dundosambrioch@gmail.com>
2023-12-07 08:12:06 -0800

    dnodeint

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,25 @@
+#include "lists."
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new = malloc(sizeof(dlistint_t)), *node;
+
+	if (!head || !new)
+		return (new ? free(new), NULL : NULL);
+	new->n = n;
+	new->next = NULL;
+	if (!*head)
+	{
+		new->prev = NULL;
+		*head = new;
+	}
+	else
+	{
+		node = *head;
+		while (node->next)
+			node = node->next;
+		node->next = new;
+		new->prev = node;
+	}
+	return (new);
+}
