b092136ce88465bc2aaf835dadedad9423c15296
Dodisambrioch <dundosambrioch@gmail.com>
2023-12-15 02:58:17 -0800

    using shared

---
+++ 0x18-dynamic_libraries/mul.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * mul - multiply two integers
+ * @a: first int
+ * @b: second int
+ * Return: multiply
+ */
+int mul(int a , int b)
+{	
+	return(a * b);
+}
